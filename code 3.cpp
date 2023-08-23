ad4b4e48f4fda785f2af5d79544f587c8ac030f0
mehmood azam <mehmoodazam556@gmail.com>
2023-08-23 11:58:51 -0700

    code 3 finalized

--- code 3.cpp
+++ code 3.cpp
@@ -1,24 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
- 
-int main () 
+int main()
 {
-  
- 
-int i = 7;
-  
-  cout<<"Enter Number " ;
- cin>>i;
- 
- 
-for (; i >= -2; i--)
-    {
-      cout << i << endl;
-    } 
-    
-return 0;
 
- 
- 
+    int i = 7;
+
+    cout << "Enter Number ";
+
+    cin >> i;
+
+    for (; i >= -2; i--)
+    {
+        if (i != 0)
+        {
+            cout << i<<endl;
+        }
+    }
+
+    return 0;
 }
