4733f474f4c0ca4f4803d21c24a19bd689b41d7f
M Hamid Jamil <m.hamid.jamil99@gmail.com>
2023-08-26 20:43:47 +0500

    Update modified code4.cpp
    new logic for printing .

--- modified code4.cpp
+++ modified code4.cpp
@@ -28,3 +28,24 @@ int main()
 
   return 0;
 }
+
+/*
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int i = 17;
+    cout << "Enter Number : ";
+    cin >> i;
+    for (; i >= 1; i--) {
+        cout << " ";
+        if (i % 2 != 0) {//odd number only
+            cout << i;
+        }
+    }
+    cout << ".";
+    return 0;
+}
+
+*/
