b59af892cd0c65f5e2e808903e6bc600486c2f17
mehmood azam <mehmoodazam556@gmail.com>
2023-08-30 12:15:12 -0700

    code 4 ,1,3

--- modified code4.cpp
+++ modified code4.cpp
@@ -5,44 +5,16 @@ int main()
 {
 
     int small;
-
-    int large;
+  int large;
+  
     cout<<"small no is?";
     cin>>small;
     cout<<"large no is?";
     cin>>large;
-    for(;large>small;large--)if(large % small !=0)
-    {
-        cout<<large;
-    }
-<<<<<<< HEAD
-;
+    if(large % 2==0)
+    large--;
+   { for(;large>=small;large-=4)
+    cout<<large<<" ";
+   }
     return 0;
 }
-=======
-  }
-
-  return 0;
-}
-
-/*
-#include <iostream>
-
-using namespace std;
-
-int main() {
-    int i = 17;
-    cout << "Enter Number : ";
-    cin >> i;
-    for (; i >= 1; i--) {
-        cout << " ";
-        if (i % 2 != 0) {//odd number only
-            cout << i;
-        }
-    }
-    cout << ".";
-    return 0;
-}
-
-*/
->>>>>>> 4733f474f4c0ca4f4803d21c24a19bd689b41d7f
