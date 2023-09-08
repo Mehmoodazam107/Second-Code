d602fc8f12ec922c806f452d3ac11af32dd6c398
mehmood azam <mehmoodazam556@gmail.com>
2023-09-08 10:28:59 -0700

    CODE7

---
+++ code7.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age[10];
+    int size=0;
+    for(;size<10;size++){cout<<"enter age of student"<<size+1;
+    cin>>age[size;]
+    }
+
+ 
+
+    return 0;
+}
